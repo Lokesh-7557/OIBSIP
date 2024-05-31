5ebdc07319e9a647ac1122bf91035e03ea44b7a3
Lokesh-7557 <iamlokesh757@gmail.com>
2024-05-31 23:49:16 +0530

    Update

---
+++ Task4.py
@@ -0,0 +1,11 @@
+#importing libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('spam.csv')
+
+print(df.head())
